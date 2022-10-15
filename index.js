b43b42283b6b94a9815d8d828b1cdd31c426f5a6
Ahsan Shah <ahsan.gemini24@gmail.com>
2022-10-15 15:24:09 +0500

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const spann = document.querySelector(".spanning");
+const span1 = document.getElementById("span1");
 const span2 = document.getElementById("span2");
 function message () {    
-span2.textContent= "Welcome To the Future! This is Metaverse!"
+span1.textContent= "Welcome To the Future! This is Metaverse!"
 console.log("yes")
 }
+
+function message2 () {    
+    span2.textContent= "Welcome Again"
+    console.log("yes")
+    }
