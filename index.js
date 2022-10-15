4b3e83b23f021ff81b463e36605f0b3bd62eb1cd
Ahsan Shah <ahsan.gemini24@gmail.com>
2022-10-15 14:22:26 +0500

    Ahsan Shah

---
+++ index.js
@@ -0,0 +1,6 @@
+const spann = document.querySelector(".spanning");
+const span2 = document.getElementById("span2");
+function message () {    
+span2.textContent= "Welcome To the Future! This is Metaverse!"
+console.log("yes")
+}
